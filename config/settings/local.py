9248940614f9ecc796426f0797159fd21ce4e604
ica1010 <ghislinametonou@gmail.com>
2025-07-05 06:50:35 +0000

    ####

--- config/settings/local.py
+++ config/settings/local.py
@@ -8,10 +8,8 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-SECRET_KEY = env(
-    "DJANGO_SECRET_KEY",
-    default="yYPmXVXwSxZVqfF7tUWNUO9iVttv6H1mVx9MHBDlvi0YO5lc76bzfo7BBwUPHGzG",
-)
+SECRET_KEY ="yYPmXVXwSxZVqfF7tUWNUO9iVttv6H1mVx9MHBDlvi0YO5lc76bzfo7BBwUPHGzG",
+
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]  # noqa: S104
 
